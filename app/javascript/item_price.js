e8b6140b92ec636dd21633c90f7af16a5c4b1e18
oberiberi <132434620+oberiberi@users.noreply.github.com>
2023-06-04 12:20:30 +0900

    販売価格

---
+++ app/javascript/item_price.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+  console.log("OK");
+const priceInput = document.getElementById("item-price");
+console.log(priceInput);
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  console.log(inputValue);
+const addTaxDom = document.getElementById("add-tax-price");
+addTaxDom.innerHTML = Math.floor(inputValue*0.1);
+const addTaxValue = parseInt(addTaxDom.innerHTML); 
+const profit = document.getElementById("profit");
+profit.innerHTML = Math.floor(inputValue - addTaxValue);
+})
+});
