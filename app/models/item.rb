61100a92b4fe97bb392b866efbaea1283200fb85
oberiberi <132434620+oberiberi@users.noreply.github.com>
2023-06-05 13:53:45 +0900

    テストコードの実装　item

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,10 +9,14 @@ class Item < ApplicationRecord
   belongs_to_active_hash :standard_send_date
   has_one_attached :image
 
-  validates :item_name, :category_id, :condition_id, :delivery_price_id, :prefecture_id, :standard_send_date_id,  presence: true
+  validates :image,:item_name, :content, :category_id, :condition_id, :delivery_price_id, :prefecture_id, :standard_send_date_id, presence: true
+  
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_price_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :delivery_price_id, numericality: { other_than: 1, message: "Delivery price can't be blank" } 
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :standard_send_date_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_998}
+
 end
--- spec/models/items_spec.rb
+++ spec/models/items_spec.rb
@@ -1,4 +1,77 @@
 require 'rails_helper'
 
-RSpec.describe Items, type: :model do
+RSpec.describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品情報入力' do
+    context '商品情報入力できる場合' do
+      it 'image, item_name, content, category, condition, delivery_price, prefecture, standard_send_date, priceが存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品情報入力できない場合' do
+      it '商品画像が必須であること' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名が必須であること' do
+        @item.item_name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+      it '商品の説明が空では登録できない' do
+        @item.content = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Content can't be blank")
+      end
+      it 'カテゴリーが必須であること' do
+        @item.category = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '商品の状態が必須であること' do
+        @item.condition = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it '配送料の負担が必須であること' do
+        @item.delivery_price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery price can't be blank")
+      end
+      it '発送元の地域が必須であること' do
+        @item.prefecture = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '発送までの日数が必須であること' do
+        @item.standard_send_date = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Standard send date can't be blank")
+      end
+      it '販売価格が必須であること' do
+        @item.price= nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '販売価格は、半角数値以外を含んでいるものは登録できない' do
+        @item.price = 'AaあBbかｶ' 
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it '販売価格が299円以下では登録できない' do
+        @item.price= 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it '販売価格が9999999円以上では登録できない' do
+        @item.price = 9999999
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999998")
+      end
+    end
+  end
 end
