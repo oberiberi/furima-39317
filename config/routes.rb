d249ea246638dac7d056c617cb2a322b8ceed7f3
oberiberi <132434620+oberiberi@users.noreply.github.com>
2023-06-06 13:17:58 +0900

    商品一覧機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  # get 'item/index'
+  # get '/items'
   resources :items, only: [:index, :new, :create]
   resources :users, only: [:show]
 end
