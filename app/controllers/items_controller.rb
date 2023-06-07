18caa320df85f9096ddc044c93c9401289ab2dcc
oberiberi <132434620+oberiberi@users.noreply.github.com>
2023-06-07 18:45:24 +0900

    商品情報編集機能　修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,25 +1,23 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
       redirect_to item_path
     else
-      render :new
+      render :edit
     end
   end
 
   def edit
-    @item = Item.find(params[:id])
     return unless current_user.id != @item.user_id
 
     redirect_to root_path
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def index
@@ -41,6 +39,10 @@ class ItemsController < ApplicationController
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:image, :item_name, :price, :category_id, :condition_id, :delivery_price_id, :prefecture_id,
