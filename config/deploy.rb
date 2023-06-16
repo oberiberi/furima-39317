c507540a2349a2d88a57cff956fd8d83e6cadc85
oberiberi <132434620+oberiberi@users.noreply.github.com>
2023-06-16 14:50:46 +0900

    mainに変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :application, 'furima-39317'
 set :repo_url,  'git@github.com:oberiberi/furima-39317.git'
 set :branch, 'main'
 
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
